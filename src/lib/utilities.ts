f44364e3eb0ececa2310cac725ec755d4d62703c
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:39 +0530

    improve(math): add isEven utility for math operations

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -4,3 +4,5 @@ export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 export const reverseString = (s: string) => s.split('').reverse().join('');
 
+export const isEven = (n: number) => n % 2 === 0;
+

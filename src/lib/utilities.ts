4295aa22d9c48c26bcc980f8f202195554598fc0
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:42 +0530

    improve(types): add isNull type guard

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -52,3 +52,5 @@ export const isArray = (val: any) => Array.isArray(val);
 
 export const isObject = (val: any) => val !== null && typeof val === 'object' && !Array.isArray(val);
 
+export const isNull = (val: any) => val === null;
+

604189f74f4453303f85566baa9a29765be3fb0f
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:41 +0530

    improve(types): add isNumber type guard

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -42,3 +42,5 @@ export const generateId = () => Math.random().toString(36).substring(2, 9);
 
 export const isString = (val: any) => typeof val === 'string';
 
+export const isNumber = (val: any) => typeof val === 'number' && !isNaN(val);
+

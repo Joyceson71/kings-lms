ce8d6cc32725835c22e90bfc5652484313e2a664
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:40 +0530

    feat(arrays): add array shuffle utility

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -32,3 +32,5 @@ export const camelCase = (str: string) => str.replace(/-([a-z])/g, (g) => g[1].t
 
 export const chunkArray = <T>(arr: T[], size: number): T[][] => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size));
 
+export const shuffleArray = <T>(arr: T[]): T[] => arr.sort(() => Math.random() - 0.5);
+

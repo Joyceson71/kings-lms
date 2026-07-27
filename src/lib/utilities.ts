65cda913f55b21da736fadec505dbf49eeabb852
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:44 +0530

    improve(security): add HTML escaping utility

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -88,3 +88,5 @@ export const getOrdinalSuffix = (i: number) => { const j = i % 10, k = i % 100; 
 
 export const bytesToSize = (bytes: number) => { const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']; if (bytes === 0) return '0 Byte'; const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)).toString()); return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i]; };
 
+export const escapeHTML = (str: string) => str.replace(/[&<>'"]/g, tag => ({'&': '&amp;', '<': '&lt;', '>': '&gt;', '\'': '&#39;', '"': '&quot;'})[tag] || tag);
+

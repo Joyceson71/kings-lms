b02a7995ad68bc3bb68227c9780abb7a492f1486
Joyceson71 <joycesondanielraj00@gmail.com>
2026-07-28 01:43:40 +0530

    feat(arrays): add array chunking utility

--- src/lib/utilities.ts
+++ src/lib/utilities.ts
@@ -30,3 +30,5 @@ export const kebabCase = (str: string) => str.replace(/([a-z])([A-Z])/g, '$1-$2'
 
 export const camelCase = (str: string) => str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
 
+export const chunkArray = <T>(arr: T[], size: number): T[][] => Array.from({ length: Math.ceil(arr.length / size) }, (v, i) => arr.slice(i * size, i * size + size));
+

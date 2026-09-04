63b8f869cc8237438372a46c8fc0223ad5d052a4
Joyceson71 <joycesondanielraj00@gmail.com>
2026-09-04 23:27:07 +0530

    fix: resolve sw.js _async_to_generator error and update pwa config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,17 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -31,4 +44,5 @@ const nextConfig: NextConfig = {
     reactCompiler: true,
   },
 };
-export default nextConfig;
+
+export default withPWA(nextConfig);
